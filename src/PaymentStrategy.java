4274f7e04bd04b7335c1281d1f4b248acfbcf74f
aaniisssaaa <aaniisssaaa@gmail.com>
2025-10-18 19:35:00 +0500

    Remove root payment example files (moved to strategy/)

--- src/PaymentStrategy.java
+++
@@ -1,9 +0,0 @@
-package strategy;
-
-public interface PaymentStrategy {
-    /**
-     * Выполнить оплату с указанной суммой.
-     * Возвращает true при успешной оплате, false при ошибке.
-     */
-    boolean pay(double amount);
-}
